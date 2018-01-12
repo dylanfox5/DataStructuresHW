6d4b2ef47bf2d388ff2f0172c72494c1d0776167
Dylan Fox <dff1@graceland.edu>
2018-01-12 11:42:14 -0600

    website update

--- Lab0HW/DylanFox.PartE.py
+++ Lab0HW/DylanFox.PartE.py
@@ -2,17 +2,14 @@
 #Write a program that iterates through all numbers <50 and determines if they are prime or not.
 
 def main(n):
-    print("0 is not prime")
-    print("1 is not prime")
     for x in range(2,n):
         isPrime = True
         for y in range(2, x):
             if x%y == 0:
                 isPrime = False
+                break
         if isPrime:
             print(x, "is prime")
-        else:
-            print(x, "is not prime")
 
 print(main(50))
 
