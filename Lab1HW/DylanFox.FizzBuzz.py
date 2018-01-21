aacdeea1af9d92a5157b4e07a7c46dd262293e56
Dylan Fox <dff1@graceland.edu>
2018-01-21 14:14:16 -0600

    website update

--- Lab1HW/DylanFox.FizzBuzz.py
+++ Lab1HW/DylanFox.FizzBuzz.py
@@ -5,10 +5,12 @@ def main():
 
     for x in range(1, 100):
         if x%3 == 0 and x%5 == 0:
-            print(x, "FizzBuzz")
+            print("FizzBuzz")
         elif x%3 == 0:
-            print(x, "Fizz")
+            print("Fizz")
         elif x%5 == 0:
-            print(x, "Buzz")
+            print("Buzz")
+        else:
+            print(x)
 
 main()
