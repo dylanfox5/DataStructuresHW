1aed5032d6f4159c27928cbbf9106a381ef70579
Dylan Fox <dff1@graceland.edu>
2018-01-22 11:52:21 -0600

    website update

--- turtleReview.py
+++ turtleReview.py
@@ -15,6 +15,7 @@ import time
 #Eugene - runs 15% faster than an average turtle, but must wait .5 second
 #to turn
 #Blaze - retired, but runs at 40% increased speed
+#Zeus the Lightning Racer - 50% faster turns, default speed
 
 class RacingTurtle:
 
@@ -22,8 +23,8 @@ class RacingTurtle:
 
         self.name = name
         self.turt = turtle.Turtle()
-        self.speed = 20 * (1 + (speed/100))
-        self.turnDelay = 0 + turnDelay
+        self.speed = speed
+        self.turnDelay = 0.3 + turnDelay
 
     def getX(self):
 
@@ -33,10 +34,10 @@ class RacingTurtle:
 
         return self.turt.ycor()
 
-    def forward(self, distance):
+    def forward(self):
         """Moves the turtle forward speed * distance"""
 
-        self.turt.forward(distance * self.speed)
+        self.turt.forward(1)
 
     def turnRight(self, degrees):
 
@@ -50,41 +51,65 @@ class RacingTurtle:
 
 racerone = RacingTurtle(0, 0, "Racer One")
 eugene = RacingTurtle(15, 0.5, "Eugene 'The Machine' Topps")
+blaze = RacingTurtle(40, 1, "Blaze")
+zeus = RacingTurtle(0, -.15, "Zeus the Lightning Turtle")
 
 eugene.turt.penup()
 eugene.turt.sety(150)
 eugene.turt.pendown()
 
-time.clock()
-eugeneStartTime = time.clock()
-
-#leg 1
-while (eugene.getX() < 100):
-       eugene.forward(1)
+blaze.turt.penup()
+blaze.turt.sety(-150)
+blaze.turt.pendown()
 
 
-#turn
-eugene.turnRight(90)
+def runRace(rt):
 
-#leg 2
-while (eugene.getY() > 50):
-       eugene.forward(1)
+    time.clock()
+    startTime = time.clock()
+    
+    #forward 27
+    #right 90
+    #forward 100
 
-eugeneEndTime = time.clock()
+    runForward(100, rt)
 
-racerStartTime = time.clock()
-#leg 1
-while (racerone.getX() < 100):
-       racerone.forward(1)
+    rt.turnRight(90)
 
-#turn
-racerone.turnRight(90)
+    runForward(100, rt)
 
-#leg 2
-while (racerone.getY() > -100):
-       racerone.forward(1)
+    endTime = time.clock()
 
-racerEndTime = time.clock()
+    return endTime - startTime
 
-print(eugeneEndTime - eugeneStartTime)
-print(racerEndTime - racerStartTime)
+def runForward(dist, rt):
+    distance = int(dist * (1 - rt.speed/100))
+    for i in range(distance):
+        rt.forward()
+
+
+
+##time.clock()
+##eugeneStartTime = time.clock()
+##
+###leg 1
+##while (eugene.getX() < 27):
+##       eugene.forward(1)
+##
+###turn
+##eugene.turnRight(90)
+##
+###leg 2
+##while (eugene.getY() > 50):
+##       eugene.forward(1)
+##
+##eugeneEndTime = time.clock()
+
+
+print(runRace(eugene))
+print(runRace(racerone))
+print(runRace(blaze))
+print(runRace(zeus))
+##print(eugeneEndTime - eugeneStartTime)
+##print(racerEndTime - racerStartTime)
+##print(blazeEndTime - blazeStartTime)
