d8c3e2561d0f0e4099982e7a053a2a2bcf12e365
Dylan Fox <dff1@graceland.edu>
2018-01-28 14:08:04 -0600

    website update

--- Lab2HW/DylanFox.Lab2.py
+++ Lab2HW/DylanFox.Lab2.py
@@ -5,62 +5,42 @@ import random
 
 class Physician:
 
-    def __init__(self, name, minutes):
+    def __init__(self, name):
+
+        self.name = name #Physician 1-6
+
+#class Nurse:
+
+class Patient:
+
+    def __init__(self, name, triaged, triageLevel, waitingRoom, ExamRoom, minutes):
 
         self.name = name
-        self.minutes = random.randint(15,20)
-
-    def setMinutes(self, minutes):
-
-        self.minutes = minutes
-
-    def getMinutes(self):
-
-        return self.minutes
-
-    def setRandMins(self):
-
-        self.setMinutes(random.randint(15,20))
+        self.traiged = triaged #boolean: True=yes; False=no
+        self.triageLevel = triageLevel #scale of 1-10? based on urgency
+        self.waitingRoom = waitingRoom #
+        self.ExamRoom = ExamRoom #Examroom patient is currently in
+        self.minutes = random.randint(15,20) #random num of mins 
 
 class ExamRoom:
     
     def __init__(self, name, physicianName, isFull, Patient):
 
-        self.name = name
-        self.physicianName = physicianName
-        self.isFull = isFull
-        self.Patient = Patient
-
-    def getIsFull(self):
-
-        return self.isFull
-
-    def setIsFull(self, isFull):
-
-        self.isFull = isFull
-##
-##class Nurse:
-##
-##    def __init__(self):
+        self.name = name #ExamRoom 1-6
+        self.physicianName = physicianName #which physician is at each examroom
+        self.isFull = isFull #boolean: true=yes; false=no
+        self.Patient = Patient #current patient in examroom
 
 
-class Patient:
-
-    def __init__(self, triaged, triageLevel, waitingRoom, ExamRoom):
-
-        self.traiged = triaged
-        self.triageLevel = triageLevel
-        self.waitingRoom = waitingRoom
-        self.ExamRoom = ExamRoom
-
-
-physician1 = Physician("Physician 1", None)
-physician2 = Physician("Physician 2", None)
-physician3 = Physician("Physician 3", None)
-physician4 = Physician("Physician 4", None)
+physician1 = Physician("Physician 1")
+physician2 = Physician("Physician 2")
+physician3 = Physician("Physician 3")
+physician4 = Physician("Physician 4")
 physicians = [physician1, physician2, physician3, physician4]
 
-patient1 = Patient(False, None, False, None)
+patient1 = Patient("Paitent 1", False, 5, False, None, 0)
+patient2 = Patient("Patient 2", False, 9, False, None, 0)
+patients = [patient1, patient2]
 
 exam1 = ExamRoom("ExamRoom 1", "", False, None)
 exam2 = ExamRoom("ExamRoom 2", "", False, None)
@@ -70,16 +50,31 @@ examrooms = [exam1, exam2, exam3, exam4]
 
 
 def simulate():
-
     for physician in physicians:
-
         for examroom in examrooms:
-
             if examroom.physicianName is "":
                 examroom.physicianName = physician.name
                 break
-        
+
+
+
+            
+    urgency = 0
+    for x in range(len(patients)):
+        if patients[x].triageLevel > urgency:
+            urgency = patients[x].triageLevel
+
+            for examroom in examrooms:
+                if examroom.Patient is None:
+                    examroom.Patient = patients[x]
+                    break
+    
+    print(patient1.minutes)    
     for examroom in examrooms:
-       print(examroom.name, examroom.physicianName)
+       if examroom.Patient is not None:
+           print(examroom.name, examroom.physicianName, examroom.Patient.name)
+       else:
+           print(examroom.name, examroom.physicianName)
+    
 
 simulate()
