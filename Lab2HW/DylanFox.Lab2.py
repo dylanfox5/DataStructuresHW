a191db9bbb9227d5ad467d7ca6407911fb1267ac
Dylan Fox <dff1@graceland.edu>
2018-01-28 21:34:23 -0600

    website update

--- Lab2HW/DylanFox.Lab2.py
+++ Lab2HW/DylanFox.Lab2.py
@@ -9,77 +9,66 @@ class Physician:
 
         self.name = name #Physician 1-6
 
+    def examination(): #sets random minutes 15-20
+        minutes = random.randint(15,20)
+
 class Nurse:
 
     def __init__(self, name):
 
         self.name = name #nurse
 
+    def getPatient(): #get patient to triage
+
+        
+    def sendPatient(): #send patient to waiting room for physician
+
 class Patient:
 
-    def __init__(self, name, triaged, triageLevel, waitingRoom, ExamRoom, minutes):
+    def __init__(self):
 
         self.name = name
-        self.triaged = triaged #boolean: True=yes; False=no
-        self.triageLevel = triageLevel #scale of 1-10? based on urgency
-        self.waitingRoom = waitingRoom #1 or 2; 1=1st waiting room, 2=2nd waiting room
-        self.ExamRoom = ExamRoom #Examroom patient is currently in
-        self.minutes = random.randint(15,20) #random num of mins 
 
 class ExamRoom:
     
-    def __init__(self, name, physicianName, isFull, Patient):
+    def __init__(self, Physician):
 
-        self.name = name #ExamRoom 1-6
-        self.physicianName = physicianName #which physician is at each examroom
-        self.isFull = isFull #boolean: true=yes; false=no
-        self.Patient = Patient #current patient in examroom
+        self.Physician = Physician #which physician is at each examroom
+        self.runTime = 0
+        self.Patient = None #current patient in examroom
 
-class WaitingRoomOne:
+    def addPatient(self, WaitingRoom): #add patient to examroom
 
-class WaitingRoomTwo:
+        self.waitingList2.pop(0)
+        
+    def removePatient(self): #remove patient from examroom
 
-
-physician1 = Physician("Physician 1")
-physician2 = Physician("Physician 2")
-physician3 = Physician("Physician 3")
-physician4 = Physician("Physician 4")
-physicians = [physician1, physician2, physician3, physician4]
-
-patient1 = Patient("Paitent 1", False, 5, 1, None, 0)
-patient2 = Patient("Patient 2", False, 9, 1, None, 0)
-patients = [patient1, patient2]
-
-exam1 = ExamRoom("ExamRoom 1", "", False, None)
-exam2 = ExamRoom("ExamRoom 2", "", False, None)
-exam3 = ExamRoom("ExamRoom 3", "", False, None)
-exam4 = ExamRoom("ExamRoom 4", "", False, None)
-examrooms = [exam1, exam2, exam3, exam4]
+        
+    def timeRemaining():
+        
+        return self.runTime
 
 
-def simulate():
-    for physician in physicians:
-        for examroom in examrooms:
-            if examroom.physicianName is "":
-                examroom.physicianName = physician.name
-                break
-            
-    urgency = 0
-    for x in range(len(patients)):
-        if patients[x].triageLevel > urgency:
-            urgency = patients[x].triageLevel
-
-            for examroom in examrooms:
-                if examroom.Patient is None:
-                    examroom.Patient = patients[x]
-                    break
     
-    print(patient1.minutes)    
-    for examroom in examrooms:
-       if examroom.Patient is not None:
-           print(examroom.name, examroom.physicianName, examroom.Patient.name)
-       else:
-           print(examroom.name, examroom.physicianName)
-    
+class WaitingRoom:
 
-simulate()
+    def __init__(self):
+
+        self.waitingList1 = []
+        self.waitingList2 = []
+
+    def addPatients(self, patient): #keep track of patients before nurse
+
+        self.waitingList1.append(patient)
+
+    def removePatients(self): #remove patiens waiting for nurse
+
+        return self.waitingList1.pop(0)
+        
+    def addPatientsTwo(self, patient): #add patients waiting for physician
+
+        self.waitingList2.append(patient)
+
+    def removePatientsTwo(self): #remove patients waiting for physician
+
+        return self.waitingList2.pop(0)
