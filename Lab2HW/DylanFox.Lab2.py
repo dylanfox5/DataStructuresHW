238ae590b8fa82a786673fd66633470ac8b65aae
Dylan Fox <dff1@graceland.edu>
2018-02-05 11:47:50 -0600

    website update

--- Lab2HW/DylanFox.Lab2.py
+++ Lab2HW/DylanFox.Lab2.py
@@ -1,5 +1,6 @@
 #Dylan Fox
 #Doctor's Office Simulation
+#https://stackoverflow.com/questions/403421/how-to-sort-a-list-of-objects-based-on-an-attribute-of-the-objects
 
 import random
 import time
@@ -17,7 +18,7 @@ examRoomSize = 6
 
 def callNurse():
     triageRoom.append(waitingRoom.pop(0)) #move patient from waiting room to triage room
-    triageRoom.sort(key=lambda Patient: Patient.triageNumber) #sorts based on triageNumber
+    triageRoom.sort(key=lambda Patient: Patient.triageNumber) #sorts based on triageNumber (See url above for lambda function)
 
 class Patient:
 
@@ -42,30 +43,40 @@ def simulate():
         the patients from the triageRoom are added to an examRoom,
         once patient's treatmentTime has expired they are removed."""
 
+    numberOfPatients = eval(input("How many patients?"))
+    examRoomSize = eval(input("How many doctors?"))
+    
     patientsTreated = 0
     minute = 0 #used to simulate real minutes without having to wait
-    while minute != 20: #number of minutes to simulate
+    while numberOfPatients != 0: #number of minutes to simulate
         print("Minute", minute+1)
         
-        if minute % 2 == 0: #adds 6 patients every 2 minutes
-            for i in range(6): 
-                waitingRoom.append(Patient(minute+1))
+        for i in range(numberOfPatients): 
+            waitingRoom.append(Patient(minute+1))
 
-        if len(triageRoom) < triageRoomSize: #max of 15 patients in triageRoom
-            if len(waitingRoom) < triageRoomSize:
-                for i in range(len(triageRoom), len(waitingRoom)): #moves to triageRoom + triages patients
-                    callNurse()
-            elif len(waitingRoom) > triageRoomSize:
-                for i in range(len(triageRoom), triageRoomSize): #moves to triageRoom + triages patients
-                    callNurse()
+        #if len(triageRoom) < triageRoomSize: #max of 15 patients in triageRoom
+
+        callNurse()
+##            if len(waitingRoom) < triageRoomSize:
+##                for i in range(len(triageRoom), len(waitingRoom)): #moves to triageRoom + triages patients
+##                    callNurse()
+##            elif len(waitingRoom) > triageRoomSize:
+##                for i in range(len(triageRoom), triageRoomSize): #moves to triageRoom + triages patients
+##                    callNurse()
 
             
-        if len(examRoom) < examRoomSize: #check any examRoom opening
+        if len(examRoom) < examRoomSize: #check any examRoom openings
             for i in range(len(examRoom), examRoomSize):
                 nextPatient = triageRoom.pop(0)
                 examRoom.append(nextPatient) #adds patient to examRoom
                 nextPatient.timeEnteredExamRoom = minute+1
 
+        for p in examRoom: #checks if treatmentTime has expired
+            if (minute - p.timeEnteredExamRoom) >= p.treatmentTime-1:
+                examRoom.remove(p) #removes patient from examRoom
+                patientsTreated += 1
+                numberOfPatients -= 1
+        
         print("Patients in Exam Room:")
         for p in examRoom:
             print(p.name, "Treatment Time:", p.treatmentTime,"Time entered Exam Room:", p.timeEnteredExamRoom)
@@ -77,15 +88,9 @@ def simulate():
         print("\nPatients in Waiting Room:")
         for p in waitingRoom:
               print(p.name)
-
-        for p in examRoom: #checks if treatmentTime has expired
-            if (minute - p.timeEnteredExamRoom) >= p.treatmentTime-1:
-                examRoom.remove(p) #removes patient from examRoom
-                patientsTreated += 1
                 
         print("Patients Treated:", patientsTreated)
         print("\n")
-        #time.sleep(3) #not actually needed
 
         minute += 1
 
