a5863bb5a5bb8523f2475dafbe1487134200efa0
Dylan Fox <dff1@graceland.edu>
2018-02-05 18:51:45 -0600

    website update

--- Lab2HW/DylanFox.Lab2.py
+++ Lab2HW/DylanFox.Lab2.py
@@ -3,7 +3,6 @@
 #https://stackoverflow.com/questions/403421/how-to-sort-a-list-of-objects-based-on-an-attribute-of-the-objects
 
 import random
-import time
 
 waitingRoom = []
 triageRoom = []
@@ -28,54 +27,51 @@ class Patient:
         self.name = names[random.randint(0,len(names)-1)]\
                     + " " + names[random.randint(0,len(names)-1)]
         self.arrivalTime = time
-        self.treatmentTime = random.randint(1, 5) 
+        self.treatmentTime = random.randint(15, 20) 
         self.timeEnteredExamRoom = 0
-        
-    def exit(self, patient): #remove patient from simulation
-        examRoom.remove(patient)
 
 def simulate():
 
-    """Simulation adds 6 patients every minute,
-        one minute is equal to one loop through the while statement,
-        patients are added to the waitingRoom, then sorted based on
-        triageNumber in the triageRoom, based on any open examRooms
-        the patients from the triageRoom are added to an examRoom,
-        once patient's treatmentTime has expired they are removed."""
+    """Simulation asks for number of patients and doctors (examRooms) based
+       on user input. The simulation runs until all patients have been treated.
+       All patients enter the waitingRoom and the nurse transfers one patient
+       at a time to the triageRoom. If there are any examRoom openings, the
+       patient is moved to an examRoom. The patiens is removed once their
+       treatment time has expired. AverageWaitTime is printed at the end."""
 
     numberOfPatients = eval(input("How many patients?"))
     examRoomSize = eval(input("How many doctors?"))
     
+    patients = numberOfPatients
     patientsTreated = 0
-    minute = 0 #used to simulate real minutes without having to wait
-    while numberOfPatients != 0: #number of minutes to simulate
-        print("Minute", minute+1)
-        
-        for i in range(numberOfPatients): 
+    minute = 0
+    averageWaitTime = 0
+    for i in range(numberOfPatients): 
             waitingRoom.append(Patient(minute+1))
-
-        #if len(triageRoom) < triageRoomSize: #max of 15 patients in triageRoom
-
-        callNurse()
-##            if len(waitingRoom) < triageRoomSize:
-##                for i in range(len(triageRoom), len(waitingRoom)): #moves to triageRoom + triages patients
-##                    callNurse()
-##            elif len(waitingRoom) > triageRoomSize:
-##                for i in range(len(triageRoom), triageRoomSize): #moves to triageRoom + triages patients
-##                    callNurse()
-
             
-        if len(examRoom) < examRoomSize: #check any examRoom openings
-            for i in range(len(examRoom), examRoomSize):
-                nextPatient = triageRoom.pop(0)
-                examRoom.append(nextPatient) #adds patient to examRoom
-                nextPatient.timeEnteredExamRoom = minute+1
+    while numberOfPatients != 0: #loops until 0 patients 
+        print("Minute", minute+1)
+
+        if len(waitingRoom) > 0:           
+            for i in range(len(waitingRoom)):
+                callNurse()
+                break
+        else:
+            pass
+           
+        if examRoomSize > 0 and len(triageRoom) > 0: #check any examRoom openings
+            nextPatient = triageRoom.pop(0)
+            examRoom.append(nextPatient) #adds patient to examRoom
+            nextPatient.timeEnteredExamRoom = minute+1
+            examRoomSize -= 1
+            averageWaitTime += (minute+1)
 
         for p in examRoom: #checks if treatmentTime has expired
             if (minute - p.timeEnteredExamRoom) >= p.treatmentTime-1:
-                examRoom.remove(p) #removes patient from examRoom
+                examRoom.remove(p)
                 patientsTreated += 1
                 numberOfPatients -= 1
+                examRoomSize += 1
         
         print("Patients in Exam Room:")
         for p in examRoom:
@@ -88,10 +84,12 @@ def simulate():
         print("\nPatients in Waiting Room:")
         for p in waitingRoom:
               print(p.name)
-                
-        print("Patients Treated:", patientsTreated)
         print("\n")
 
         minute += 1
+        
+    print("Patients Treated:", patientsTreated)
+    print("Average Wait Time:", averageWaitTime/patients)
 
 simulate()
+
