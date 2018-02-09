05efa0b3b91cefdeb7ce70a75f78957736798b77
Dylan Fox <dff1@graceland.edu>
2018-02-09 14:49:18 -0600

    website update

---
+++ Lab4HW/DylanFox.Lab4.partA.py
@@ -0,0 +1,25 @@
+#Dylan Fox
+#Collatz Conjecture
+
+def calculate(integer, x):
+
+    steps = x
+
+    if integer == 1:
+        steps += 1
+        print("It takes",steps, "steps.")
+        return 1
+    else:
+        if integer%2 == 0:
+            steps += 1
+            return calculate(integer/2, steps)
+        else:
+            steps += 1
+            return calculate((integer*3)+1, steps)
+
+def main():
+    integer = eval(input("Enter a positive integer:"))
+    calculate(integer, 0)
+
+main()
+
