fed07ebf3dbdbc2c662d24733549612b710f1698
Dylan Fox <dff1@graceland.edu>
2018-02-26 00:25:24 -0600

    website update

--- Lab5HW/DylanFox.Lab5HW.py
+++ Lab5HW/DylanFox.Lab5HW.py
@@ -1,31 +1,35 @@
 #Dylan Fox
 #Dictionary Search
 
-
-file = open("wordlist.txt", "r")
-
-#lines = file.read().split(",")
-
-words = ["hello", "my", "name", "is", "Dylan", "Fox"]
-
+import wordlist
 
 def linearSearch(word):
 
     index = 0
 
-    for line in file:
-        for entry in line.split():
-            if entry == word:
-                return index
-            else:
-                index += 1
+    for entry in wordlist.words:
+        if entry == word:
+            return index
+        else:
+            index += 1
 
-##    for entry in words:
-##        if entry == word:
-##            return index
-##        index += 1
-                                    
-#print(linearSearch("hello"))
 print(linearSearch("abbot"))
 
+def binaryRecursive(word, x):
 
+    index = 0
+    middle = int((len(wordlist.words) - 1)/2)
+    item = wordlist.words[middle]
+
+    if item == word:
+        return index
+
+    elif item>word:
+        index += 1
+        return binaryRecursive([0, middle], index)
+    
+    elif item<word:
+        index += 1
+        return binaryRecursive([middle], index)
+    
+print(binaryRecursive("abbot", 0))
